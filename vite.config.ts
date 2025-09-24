0d4a52f92388e78c46f5194c0200c57220724110
Enigma1212 <meranomystery@gmail.com>
2025-09-24 08:31:01 +0200

    chore: pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'   // <— NEU
 
 export default defineConfig({
-  base: './https://github.com/HofDa/bnature/',  // Repo-Namen anpassen
+  base: "/bnature/", 
   plugins: [react(), tailwindcss()],          // <— NEU
 })
